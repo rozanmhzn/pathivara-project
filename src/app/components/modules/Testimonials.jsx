85cc60a3716af6e6b414cd74dec6c4949f13497e
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2024-06-11 22:35:45 +0545

    Fixed horizontal scroll on testionomals

--- src/app/components/modules/Testimonials.jsx
+++ src/app/components/modules/Testimonials.jsx
@@ -1,15 +1,62 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import user from "../../assets/user.jpg";
 import { productionData } from "@/app/constant/productionData";
-
-import {
-  IoMdArrowDroprightCircle,
-  IoMdArrowDropleftCircle,
-} from "react-icons/io";
+import { IoMdArrowDroprightCircle, IoMdArrowDropleftCircle } from "react-icons/io";
 import ProductionCard from "../ProductionCard";
+import styled, { createGlobalStyle } from "styled-components";
+import React from "react";
+import HoriztionalScroll from "../../components/horiztionalScroll";
 
+const Main = styled.main``;
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+`;
+const HorizontalSection = styled.section`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const BumperSection = styled.section`
+  text-align: center;
+  padding: 128px 16px;
+  background-color: #efefef;
+`;
+
+const CardsContainer = styled.div`
+  position: relative;
+  height: 100%;
+  padding: 0 0 0 150px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SampleCard = styled.div`
+  overflow-y: hidden;
+  position: relative;
+  height: 300px;
+  width: 500px;
+  background-color: #111f30;
+  margin-right: 75px;
+  flex-shrink: 0;
+`;
+
+const SampleCards = React.memo(() =>
+  Array(5)
+    .fill(0)
+    .map((_e, i) => <SampleCard key={`sampleCard-${i}`} />)
+);
 const Testimonials = () => {
   return (
     <div className="mt-10">
@@ -24,12 +71,10 @@ const Testimonials = () => {
       </section>
       <section className=" mx-5 bg-bgColor p-7">
         <div className="flex flex-col gap-5 items-center">
-          <span className="text-lg font-semibold">
-            Best IT Company in Nepal
-          </span>
+          <span className="text-lg font-semibold">Best IT Company in Nepal</span>
           <span className="text-textColor text-xl w-3/4">
-            I have been using LMS for my engineering college which was developed
-            by Pathivara Innovations. Very happy with their product and support.
+            I have been using LMS for my engineering college which was developed by Pathivara Innovations. Very happy
+            with their product and support.
           </span>
           <section className="mt-5 flex justify-center items-center gap-3">
             <Image className="rounded-full" width={50} height={50} src={user} />
@@ -51,24 +96,27 @@ const Testimonials = () => {
         </div>
         <div className="grid justify-items-center">
           <span className="text-sm font-semibold">WHAT PEOPLE ARE SAYING</span>
-          <span className="text-sm text-textColor font-semibold">
-            TESTIMONIALS
-          </span>
+          <span className="text-sm text-textColor font-semibold">TESTIMONIALS</span>
         </div>
       </section>
-      <section className="flex mt-10 mx-5 gap-5 w-full ">
-        <div className="text-HeadingColor w-3/5 text-9xl">
-          <span>WHAT WE CAN DO</span>
-        </div>
-        <div className=" flex flex-row gap-5 h-[100vh]">
-          {productionData.slice(0, 6).map((data, index) => {
-            return <ProductionCard key={index} cardData={data} />;
-          })}
-        </div>
-        <div className="text-HeadingColor w-3/5 text-9xl">
-          ALL IT SOLUTIONS HERE
-        </div>
-      </section>
+      <HorizontalSection>
+        <HoriztionalScroll>
+          <CardsContainer>
+            <section className="flex mt-10 mx-5 gap-5 w-full overflow-y-hidden relative h-[90vh] ">
+              <div className="text-HeadingColor w-3/5 text-9xl">
+                <span>WHAT WE CAN DO</span>
+              </div>
+
+              <div className=" flex flex-row gap-5 h-[100vh]">
+                {productionData.slice(0, 6).map((data, index) => {
+                  return <ProductionCard key={index} cardData={data} />;
+                })}
+              </div>
+              <div className="text-HeadingColor w-3/5 text-9xl">ALL IT SOLUTIONS HERE</div>
+            </section>
+          </CardsContainer>
+        </HoriztionalScroll>
+      </HorizontalSection>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Navbar from "./components/partials/Navbar";
 import SideNavbar from "./components/partials/SideNavbar";
@@ -10,38 +11,53 @@ import OurClients from "./components/modules/OurClients";
 import Footer from "./components/partials/Footer";
 import ImageSlider from "./components/ImageSlider";
 import HeroSection from "./components/modules/HeroSection";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
 
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+`;
 export default function Home() {
   return (
-    <main>
-      <section className="navbar">
-     <Navbar/>
-      </section>
-      <section>
-        <HeroSection/>
-      </section>
-      <section className="static">
-        <AboutUs/>
-      </section>
-      <section>
-        <OurServices/>
-      </section>
-      <section>
-        <OurMissions/>
-      </section>
-      <section>
-        <Testimonials/>
-      </section>
-      <section>
-        <LatestNews/>
-      </section>
-      <section>
-        <OurClients/>
-      </section>
-      <section><Footer/></section>
-      <section className="side-bar">
-        <SideNavbar/>
-      </section>
-    </main>
+    <>
+      <GlobalStyle />
+      <main>
+        <section className="navbar">
+          <Navbar />
+        </section>
+        <section className="h-[100vh]">
+          <HeroSection />
+        </section>
+        <section className="static">
+          <AboutUs />
+        </section>
+        <section>
+          <OurServices />
+        </section>
+        <section>
+          <OurMissions />
+        </section>
+        <section>
+          <Testimonials />
+        </section>
+        <section>
+          <LatestNews />
+        </section>
+        <section>
+          <OurClients />
+        </section>
+        <section>
+          <Footer />
+        </section>
+        <section className="side-bar">
+          <SideNavbar />
+        </section>
+      </main>
+    </>
   );
 }
